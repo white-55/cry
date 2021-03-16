2805368e0416f408ce52187232ff62281283f158
zry <771079578@qq.com>
2021-03-16 22:46:22 +0800

    first

---
+++ shop-home/src/store/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+import {state,mutations,getters} from './mutations'
+import actions from './actions'
+
+import menu from './modules/menu'
+import role from './modules/role'
+import manager from './modules/manager'
+import cate from './modules/cate'
+import spec from './modules/spec'
+
+
+const store = new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters,
+  modules:{
+    menu,
+    role,
+    manager,
+    cate,
+    spec
+  }
+})
+
+export default store
